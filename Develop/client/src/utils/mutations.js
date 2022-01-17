3e1035e1b4c4c22e41a5217516decbff964f9228
Cienna97 <cmortenson97@gmail.com>
2022-01-17 01:48:38 -0700

    export addUser and loginUser

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,8 +1,32 @@
-
+import { gql } from '@apollo/client';
 //LOGIN_USER execute loginUser mutation
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 //ADD_USER execute addUser mutation
 
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email:
+    String!, $password: String!) {
+        addUser(username: $username, email:
+            $email, password: $password) {
+                token
+                user {
+                    _id
+                    username
+                }
+            }
+        }
+`;
 //SAVE_BOOK execute saveBook mutation
 
 //REMOVE_BOOK execute removeBook mutation
