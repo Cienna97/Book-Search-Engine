60b02fefa7b1b01380bc05fc2f54773ae5093ce4
Cienna97 <cmortenson97@gmail.com>
2022-01-17 18:38:51 -0700

    export and logout, auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -34,9 +34,17 @@ class AuthService {
   login(idToken) {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
-
+  }
+  
+  logout() {
+    // clear user from localStorage
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
 }
 
+
+export default new AuthService();
 // set token secret and expiration date
 //const secret = 'mysecretsshhhhh';
 //const expiration = '2h';
