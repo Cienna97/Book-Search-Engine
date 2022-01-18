ec2840379245c91fcf82a228ee1cbc46fb8788a9
Cienna97 <cmortenson97@gmail.com>
2022-01-17 18:52:00 -0700

    working on client app.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,83 +1,34 @@
-import decode from 'jwt-decode';
+const jwt = require('jsonwebtoken');
 
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
-//create class user
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
-
-  //check if user is loggin into application
-  loggedIn() {
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
-  }
-
-  // check if token is expired
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now()/1000) {
-        return true;
-      } else return false;
-    } catch (err){
-      return false;
-    }
-  }
-
-  //get user token from local storage
-  getToken() {
-    return localStorage.getItem('id_token');
-  }
-
-  // saves user's token to localStorage
-  login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
-  
-  logout() {
-    // clear user from localStorage
-    localStorage.removeItem('id_token');
-    window.location.assign('/');
-  }
-}
-
-
-export default new AuthService();
-// set token secret and expiration date
-//const secret = 'mysecretsshhhhh';
-//const expiration = '2h';
-
-//module.exports = {
-  // function for our authenticated routes
-  //authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    //let token = req.query.token || req.headers.authorization;
+module.exports = {
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
-    //if (req.headers.authorization) {
-      //token = token.split(' ').pop().trim();
-    //}
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-    //if (!token) {
-      //return res.status(400).json({ message: 'You have no token!' });
-    //
-    // verify token and get user data out of it
-    //try {
-      //const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      //req.user = data;
-    //} catch {
-      //console.log('Invalid token');
-      //return res.status(400).json({ message: 'invalid token!' });
-    //}
+    if (!token) {
+      return req;
+    }
 
-    // send to next endpoint
-    //next();
-  //},
-  //signToken: function ({ username, email, _id }) {
-    //const payload = { username, email, _id };
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
 
-    //return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  //},
-//};
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
